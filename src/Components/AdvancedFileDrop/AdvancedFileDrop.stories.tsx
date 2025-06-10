2678fc3098e7c720833b5a2013cc6b193c6b5114
Praveen Verma <praveenstp2003@gmail.com>
2025-06-10 15:30:25 +0530

    change in file drop

--- src/Components/AdvancedFileDrop/AdvancedFileDrop.stories.tsx
+++ src/Components/AdvancedFileDrop/AdvancedFileDrop.stories.tsx
@@ -19,10 +19,3 @@ export const Empty: Story = {
   args: {},
 };
 
-export const WithFiles: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    // Simulate files added - we can't programmatically add files easily without input events
-    // But you can modify component to accept initial files prop for this story if you want
-  },
-};
